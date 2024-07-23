5ab4cc032b2ec4d701ed0fb1e9261237d1229ccc
Max Seeliger <maximilian.seeliger@gmail.com>
2024-07-23 15:03:19 +0200

    Adjusted error from merging

--- src/main.py
+++ src/main.py
@@ -121,30 +121,3 @@ def execute(config_path: str, input: str, parser_method):
         if not output_path_exists:
             os.remove(config["output_path"])
     return sat, model
-
-
-
-if __name__ == "__main__":
-    from argparse import ArgumentParser
-    parser = ArgumentParser()
-    parser.add_argument("--smt2", type=str, help="Path to the smt2 file")
-    parser.add_argument("--readable", type=str, help="Path to the readable file")
-    parser.add_argument("--config", type=str, required=True, help="Path to the config file")
-    args = parser.parse_args()
-
-    if args.smt2:
-        with open(args.smt2, "r") as file:
-            smt2 = file.read()
-        is_sat, model = execute_smt2(args.config, smt2)
-    elif args.readable:
-        with open(args.readable, "r") as file:
-            readable = file.read()
-        is_sat, model = execute_readable(args.config, readable)
-    else:
-        raise ValueError("Either --smt2 or --readable must be provided")
-    
-    print(f"The system is {is_sat.name}")
-    if is_sat is Result.SAT:
-        print("Model:")
-        for var, value in model.items():
-            print(f"{var}: {value}")
